2f5fbb6269ce58cdc3c7d71b7e74c6221c2c782e
assouljawad97-lang <assouljawad97@gmail.com>
2026-03-07 00:26:41 +0100

    Delete ocr-app directory

--- ocr-app/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('ocrApi', {
-  openFileDialog: () => ipcRenderer.invoke('dialog:openFile'),
-  processOCR: (payload) => ipcRenderer.invoke('ocr:process', payload),
-  getPreferredLanguages: () => ipcRenderer.invoke('prefs:getLanguages'),
-  saveTextResult: (payload) => ipcRenderer.invoke('dialog:saveText', payload)
-});
